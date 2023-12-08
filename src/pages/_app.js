a6bb8810918d502e6c2cacaf45faab790c0da4fe
termofday <you@example.com>
2023-12-08 17:53:00 +0100

    überarbeitet

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+  <Head>
+
+<title>Markdown Renderer with React/NextJS</title>
+<meta name='description' content='A Markdown Renderer with React/NextJS.'></meta>
+
+  </Head>
+  
+  <Component {...pageProps} />
+  
+  </>
+)}
